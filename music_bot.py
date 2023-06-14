e7fa49edaa07ca5418c12e19f0c78f91bd8ab35d
wadimilian <56312460+wadimilian@users.noreply.github.com>
2023-06-14 17:52:34 +0200

    Update music_bot.py

--- music_bot.py
+++ music_bot.py
@@ -1,114 +1,16 @@
-import logging
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
-from dejavu import Dejavu
-from dejavu.recognize import FileRecognizer
+from telegram.ext import Updater, CommandHandler
 
-# Встановлення логування
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# Конфігурація для Dejavu
-config = {
-    "database": {
-        "host": "localhost",
-        "user": "your_username",
-        "password": "your_password",
-        "database": "your_database"
-    },
-    "database_type": "mysql"
-}
-
-# Створення екземпляру Dejavu
-djv = Dejavu(config)
-
-# Оголошення функцій
-
-# Функція для обробки команди /start
+# Функція, яка буде виконуватися при отриманні команди /start
 def start(update, context):
-    user = update.effective_user
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Привіт, {user.first_name}!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привіт! Я бот привітання. Як справи?")
 
-# Функція для обробки команди /help
-def help(update, context):
-    help_message = """
-    Це бот для аналізу музики та формування плейлистів.
-    Він має такі команди:
-    - /start: Початок роботи з ботом
-    - /help: Вивести цей довідник
-    - /analyze: Аналізувати музику в каналі та сформувати плейлисти
-    """
-    context.bot.send_message(chat_id=update.effective_chat.id, text=help_message)
-
-# Функція для аналізу музики та формування плейлистів
-def analyze_music(update, context):
-    chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Аналізую музику...")
-
-    # Отримати список аудіофайлів з телеграм-каналу
-
-    # Наприклад, використовуючи python-telegram-bot API, ви можете отримати список файлів з повідомленнями у каналі
-    # Потім ви можете завантажити ці файли та аналізувати їх використовуючи Dejavu
-
-    # Приклад коду для отримання файлів з каналу:
-    # messages = context.bot.get_chat_history(chat_id=CHANNEL_ID, limit=10)
-    # for message in messages:
-    #     if message.document:
-    #         file_id = message.document.file_id
-    #         file = context.bot.get_file(file_id)
-    #         file.download('music_files/' + file.file_path.split('/')[-1])
-
-    # Аналіз музики та формування плейлистів
-
-    # Наприклад, використовуючи Dejavu, ви можете проаналізувати кожен завантажений файл:
-    # for filename in os.listdir('music_files/'):
-    #     song = djv.recognize(FileRecognizer, 'music_files/' + filename)
-    #     if song:
-    #         # Отримати назву пісні, виконавця та іншу інформацію
-    #         title = song['song_name']
-    #         artist = song['artist_name']
-    #         # Додати пісню до відповідного плейлисту або створити новий плейлист
-
-    # Відправка плейлистів користувачу
-
-    # Наприклад, використовуючи InlineKeyboardMarkup, ви можете створити кнопки для кожного плейлисту та відправити їх користувачу
-    # keyboard = [
-    #     [InlineKeyboardButton("Плейлист 1", callback_data='playlist1')],
-    #     [InlineKeyboardButton("Плейлист 2", callback_data='playlist2')],
-    #     ...
-    # ]
-    # reply_markup = InlineKeyboardMarkup(keyboard)
-    # context.bot.send_message(chat_id=chat_id, text="Ось ваші плейлисти:", reply_markup=reply_markup)
-
-# Функція для обробки натискань на кнопки
-def button_click(update, context):
-    query = update.callback_query
-    data = query.data
-
-    # Дії в залежності від натиснутої кнопки
-    if data == 'playlist1':
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Ви вибрали плейлист 1.")
-    elif data == 'playlist2':
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Ви вибрали плейлист 2.")
-    ...
-
-    # Оновлення повідомлення з кнопками (видалення кнопок)
-    query.edit_message_reply_markup(reply_markup=None)
-
-# Основна функція для обробки отриманих повідомлень
 def main():
     # Створення екземпляру Updater з токеном вашого бота
-    updater = Updater(token='5898259885:AAGXE1goXG-4uD_XU9w3JyzhI2d9aVZNuUs', use_context=True)
+    updater = Updater(token='YOUR_BOT_TOKEN', use_context=True)
     dispatcher = updater.dispatcher
 
-    # Реєстрація обробників команд
+    # Реєстрація обробника команди /start
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help))
-    dispatcher.add_handler(CommandHandler("analyze", analyze_music))
-
-    # Реєстрація обробника натискань на кнопки
-    dispatcher.add_handler(CallbackQueryHandler(button_click))
 
     # Запуск бота
     updater.start_polling()
